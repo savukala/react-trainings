0ab418adf8b515820677b04cfc4b9ab38eb4c8c8
jjuv <jesse.juvonen@gmail.com>
2021-05-30 17:46:30 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import AddTodo from './AddTodo';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  
+  
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const fetchItems = () => {
+    fetch('https://todolist-e4293-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
+    .then(response => response.json())
+    .then(data => addKeys(data))
+    .catch(err => console.error(err))
+  }
+  // Add keys to the todo objects
+  const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((item, index) => 
+    Object.defineProperty(item, 'id', {value: keys[index]}));
+    setTodos(valueKeys);
+  }
+
+  const addTodo = (newTodo) => {
+    fetch('https://todolist-e4293-default-rtdb.europe-west1.firebasedatabase.app/items/.json',
+    {
+      method: 'POST',
+      body: JSON.stringify(newTodo)
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
+  const deleteTodo = (id) => {
+    fetch(`https://todolist-e4293-default-rtdb.europe-west1.firebasedatabase.app/items/${id}.json`,
+   {
+      method: 'DELETE',
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" noWrap>
+            TodoList
+          </Typography>
+        </Toolbar>
+      </AppBar> 
+      <AddTodo addTodo={addTodo}/>
+      <div className="ag-theme-material" style={ { height: 400, width: 600, margin: 'auto' } }>
+        <AgGridReact rowData={todos}>
+          <AgGridColumn sortable={true} filter={true} field='description' />
+          <AgGridColumn sortable={true} filter={true} field='date' />
+          <AgGridColumn sortable={true} filter={true} field='priority' />
+          <AgGridColumn 
+            headerName=''
+            field='id' 
+            width={90}
+            cellRendererFramework={ params => 
+              <IconButton onClick={() => deleteTodo(params.value)} size="small" color="secondary">
+                <DeleteIcon />
+              </IconButton>
+            }
+          />      
+        </AgGridReact>
+      </div>
     </div>
-  );
+   );
 }
 
 export default App;
