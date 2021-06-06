aeeb6ba42b7d7300d720e62f8ad93ad7e37ecb29
jjuv <jesse.juvonen@gmail.com>
2021-06-06 12:37:12 +0300

    simplecalculator init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [calc, setCalc] = useState({firstvalue: '', secondvalue: '', result: ''});
+
+  const inputChanged = (event) => {
+    setCalc({...calc, [event.target.name]: event.target.value});
+  }
+  const addResult = (event) => {
+    let result = Number(calc.firstvalue) + Number(calc.secondvalue);
+    console.log(result);
+    setCalc({...calc, [event.target.name]: result});
+  }
+  const subResult = (event) => {
+    let result = Number(calc.firstvalue) - Number(calc.secondvalue);
+    console.log(result);
+    setCalc({...calc, [event.target.name]: result});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Result: {calc.result} <br />
+    
+      <input placeholder="First value" name="firstvalue" value={calc.firstvalue} onChange={inputChanged} />
+      <input placeholder="Second value" name="secondvalue" value={calc.secondvalue} onChange={inputChanged} />
+      <button name="result" onClick={addResult}>+</button>
+      <button name="result" onClick={subResult}>-</button>
+    
     </div>
   );
 }
